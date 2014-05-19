0d7b52010db53199edf66435c03ac92909d2fc28
Tim Chaptykov <tim.chaptykov@gmail.com>
2014-05-19 14:18:04 +0700

    Thumb border transition duration fix

--- src/photor.js
+++ src/photor.js
@@ -460,7 +460,7 @@
                     current = p.galleryThumbs && p.galleryThumbs[target],
                     thumbsW = p.thumbs.outerWidth(),
                     layerW = p.thumbsLayer.outerWidth(),
-                    delay = noEffects ? '0s' : '.24s',
+                    delay = noEffects ? '0s' : (p.params.delay * 0.0008) + 's',
                     indent, validatedIndent;
 
                 p.thumbsDragging = thumbsW < layerW;
